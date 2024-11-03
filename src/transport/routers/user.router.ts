fb66cdb33437b5e23d79abbe9f31dbc58ba82c97
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2024-11-03 21:18:57 +0700

    create UserRouter

---
+++ src/transport/routers/user.router.ts
@@ -0,0 +1,10 @@
+import { Router as ExpressRouter } from 'express';
+
+import { Router } from '@/shared/libs/router.lib';
+import { UserController } from '@/app/controllers';
+
+export class UserRouter extends Router<UserController> {
+	constructor(router: ExpressRouter) {
+		super(router, '/user', new UserController());
+	}
+}
