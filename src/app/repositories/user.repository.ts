f9dcde551020f118a8b003d80a107f375bd56293
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2024-11-03 21:42:14 +0700

    create findOneByEmail repo

--- src/app/repositories/user.repository.ts
+++ src/app/repositories/user.repository.ts
@@ -1,7 +1,33 @@
+import { Response } from 'express';
+
 import { Repository } from '@/shared/libs/repository.lib';
+import { User } from '@/app/models/user.model';
 
 export class UserRepository extends Repository {
 	constructor() {
 		super();
+	}
+
+	/**
+	 * Find One by Email
+	 *
+	 * @param email
+	 * @returns
+	 */
+	public async findOneByEmail(
+		res: Response,
+		email: string,
+	): Promise<User | null> {
+		let result: User | null = null;
+
+		try {
+			result = await User.findOne({
+				where: { email },
+				attributes: ['id', 'email'],
+			});
+		} catch (error) {
+			await this.catchErrorHandler(res, error, this.findOneByEmail.name);
+		}
+		return result;
 	}
 }
