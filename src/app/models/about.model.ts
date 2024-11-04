51293094d5493027660b61fe909f18dcae758f94
Bhakti Mega Buana <bhakti@orderonline.id>
2024-11-04 15:46:14 +0700

    create about model

---
+++ src/app/models/about.model.ts
@@ -0,0 +1,76 @@
+import {
+	DataTypes,
+	InferAttributes,
+	InferCreationAttributes,
+	Model,
+	Sequelize,
+} from 'sequelize';
+
+import { MySQL } from '@/shared/utils';
+
+export class About extends Model<
+	InferAttributes<About>,
+	InferCreationAttributes<About>
+> {
+	public id?: number;
+	public name!: string;
+	public title!: string;
+	public summary_en!: string;
+	public summary_id!: string;
+	public is_active?: boolean;
+	public created_at?: Date;
+	public updated_at?: Date;
+	public deleted_at?: Date | null;
+}
+
+About.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+			allowNull: false,
+		},
+		name: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+		},
+		title: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+		},
+		summary_en: {
+			type: DataTypes.TEXT(),
+			allowNull: false,
+		},
+		summary_id: {
+			type: DataTypes.TEXT(),
+			allowNull: false,
+		},
+		is_active: {
+			type: DataTypes.BOOLEAN,
+			allowNull: false,
+			defaultValue: false,
+		},
+		created_at: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: Sequelize.literal('NOW()'),
+		},
+		updated_at: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: Sequelize.literal('NOW()'),
+		},
+		deleted_at: {
+			type: DataTypes.DATE,
+			allowNull: true,
+		},
+	},
+	{
+		tableName: 'abouts',
+		freezeTableName: false,
+		timestamps: false,
+		sequelize: MySQL.getMainDbConnection(),
+	},
+);
