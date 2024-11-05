465f44a75465860a0ae43cea5f372af8f7afe4aa
Bhakti Mega Buana <bhaktibuana19@gmail.com>
2024-11-05 23:40:02 +0700

    AboutRepo create about

--- src/app/repositories/about.repository.ts
+++ src/app/repositories/about.repository.ts
@@ -1,7 +1,34 @@
+import { Response } from 'express';
+import { InferCreationAttributes, Transaction } from 'sequelize';
+
 import { Repository } from '@/shared/libs/repository.lib';
+import { About } from '@/app/models';
 
 export class AboutRepository extends Repository {
 	constructor() {
 		super();
+	}
+
+	/**
+	 * Create about
+	 *
+	 * @param res
+	 * @param payload
+	 * @param transaction
+	 * @returns
+	 */
+	public async create(
+		res: Response,
+		payload: InferCreationAttributes<About>,
+		transaction: Transaction | null = null,
+	): Promise<About | null> {
+		let result: About | null = null;
+
+		try {
+			result = await About.create(payload, { transaction });
+		} catch (error) {
+			await this.catchErrorHandler(res, error, this.create.name);
+		}
+		return result;
 	}
 }
